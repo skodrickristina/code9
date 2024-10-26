db785ab30be7299d08bdc28996367e06f640f52d
skodrickristina <skodrickristina@gmail.com>
2024-10-26 13:01:07 +0200

    Fix reference.

--- terraform/s3-site/output.tf
+++ terraform/s3-site/output.tf
@@ -1,6 +1,6 @@
 # Show me bucket url
 output "bucket_url" {
-  value = "http://${aws_s3_bucket_website_configuration.code9-bucket-vlada-code9-clever-foxhound.website_endpoint}"
+  value = "http://${aws_s3_bucket_website_configuration.my-static-website.website_endpoint}"
 }
 
 output "aws_region" {
